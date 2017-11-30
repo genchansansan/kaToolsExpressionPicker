386db8c65b81eff8f05963465286d3527fa17e64
Unknown <what.you.and.i.both.love@gmail.com>
2017-11-30 02:00:58 -0800

    adding syntax highlighting for string. fixed single line highlighting

--- python2.7libs/kaToolsExpressionPicker/syntaxRules.py
+++ python2.7libs/kaToolsExpressionPicker/syntaxRules.py
@@ -2,12 +2,13 @@ from PySide2 import QtWidgets, QtCore, QtGui
 
 
 ###  char formats
-__varTypeFormat = QtGui.QTextCharFormat()
-#__varTypeFormat.setFontWeight(QtGui.QFont.Bold)
-__varTypeFormat.setForeground(QtCore.Qt.magenta)
+__varFormat = QtGui.QTextCharFormat()
+#__varFormat.setFontWeight(QtGui.QFont.Bold)
+__varFormat.setForeground(QtCore.Qt.magenta)
 
-__varCommentFormat = QtGui.QTextCharFormat()
-__varCommentFormat.setForeground(QtCore.Qt.yellow)
+
+__commentFormat = QtGui.QTextCharFormat()
+__commentFormat.setForeground(QtCore.Qt.yellow)
 
 
 
@@ -16,19 +17,22 @@ __varTypePatterns = ["int", "float", "string", "vector"
     , "matrix", "matrix3", "vector4", "vector2", "matrix2"]
 __varTypePatterns = ["\\b" + pattern + "\\b" for pattern in __varTypePatterns]
 
-__singleLinePattern = ["//"]
-__singleLinePattern = [pattern + "*" for pattern in __singleLinePattern]
 
-__multilinePatternsStart = ["/\\*"]
-__multilinePatternsEnd = ["\\*/"]
+
+__singleLinePattern = ["//"]
+#__singleLinePattern = [pattern + "" for pattern in __singleLinePattern]
+
+__multilinePatternsStart = ["/\\*", "\"", "'"]
+__multilinePatternsEnd = ["\\*/", "\"", "'"]
 
 # ("pattern", format)
 vexRules = []
-[vexRules.append((pattern, __varTypeFormat)) for pattern in __varTypePatterns]
+[vexRules.append((pattern, __varFormat)) for pattern in __varTypePatterns]
+
 
 singleLineRules = []
-[singleLineRules.append((pattern, __varCommentFormat)) for pattern in __singleLinePattern]
+[singleLineRules.append((pattern, __commentFormat)) for pattern in __singleLinePattern]
 
 
 multiLineRules = []
-[multiLineRules.append((start, end, __varCommentFormat)) for (start, end) in zip(__multilinePatternsStart, __multilinePatternsEnd)]
+[multiLineRules.append((start, end, __commentFormat)) for (start, end) in zip(__multilinePatternsStart, __multilinePatternsEnd)]
